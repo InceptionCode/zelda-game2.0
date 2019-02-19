4948d9555ad265b01b4c5f29bf50d6fd51a320e4
Darrell Washington <darrellwjr.com@gmail.com>
2019-02-19 02:54:11 -0500

    squashed and merged in feature/start-page

---
+++ src/components/StartPage/Start.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import zeldaImage from '../../images/zelda-image.png'
+
+const Start = props => {
+  const [introVidTime, setIntroVidTime] = useState(0)
+
+  const increaseIntroVidTime = () => {
+    setIntroVidTime(introVidTime + 1)
+  }
+
+  useEffect(() => {
+    if (introVidTime === 10) return
+    const timer = window.setTimeout(increaseIntroVidTime, 1000)
+    return () => clearTimeout(timer)
+  }, [introVidTime])
+
+  const displayOptionsMenu =
+    introVidTime === 10 ? (
+      <div className="start-menu">
+        <button
+          className="start-button"
+          onClick={() => props.changePage('intro')}
+        >
+          Start
+        </button>
+        <button
+          className="credits-button"
+          onClick={() => props.changePage('credits')}
+        >
+          Roll Credits
+        </button>
+      </div>
+    ) : (
+      <h2>History of Zelda</h2>
+    )
+
+  return (
+    <div id="start-menu" data-testid="start-page">
+      <h1 className="title">
+        Zelda: The <strong>HERO</strong>
+        <i className="fa fa-shield" aria-hidden="true" />
+      </h1>
+      <img className="zelda-image" src={zeldaImage} alt="" />
+      {displayOptionsMenu}
+      <iframe
+        className="zelda-history"
+        title="zelda-history-video"
+        src="https://www.youtube.com/embed/9C064fZFKrQ?&autoplay=1&mute=1&"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  )
+}
+
+export default Start
---
+++ src/components/StartPage/Start.test.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement
+} from 'react-testing-library'
+
+import Start from './Start'
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup)
+
+describe('<Start/>', () => {
+  let mockChangePage, mockDisplayMessage, getByTestId
+  beforeEach(() => {
+    mockChangePage = jest.fn()
+    mockDisplayMessage = jest.fn()
+    getByTestId = render(
+      <Start changePage={mockChangePage} displayMessage={mockDisplayMessage} />
+    ).getByTestId
+  })
+
+  it('should render with no problem.', () => {
+    expect(getByTestId('start-page')).toBeTruthy()
+  })
+
+  it('should render <h2> title if introVid state !== 10.', () => {
+    expect(getByTestId('start-page').querySelector('h2').innerHTML).toEqual(
+      'History of Zelda'
+    )
+  })
+
+  jest.useFakeTimers()
+  jest.advanceTimersByTime(5000)
+  // Cannot figure out how to test after 10 seconds
+  it('should render two buttons, one starts the game the other roll credits when state.introVidTime === 10.', async () => {
+    const { getByTestId, getByText } = render(
+      <Start changePage={mockChangePage} displayMessage={mockDisplayMessage} />
+    )
+
+    setTimeout(async () => {
+      const startPage = getByTestId('start-page')
+      const startButton = await waitForElement(() =>
+        getByText(startPage, 'Start')
+      )
+      const creditsButton = await waitForElement(() =>
+        getByText(startPage, 'Roll Credits')
+      )
+
+      expect(startButton).toBeTruthy()
+      expect(creditsButton).toBeTruthy()
+    }, 5000)
+  })
+
+  describe('Button functionality', () => {
+    let getByTestId, getByText
+    beforeEach(() => {
+      const component = render(
+        <Start
+          changePage={mockChangePage}
+          displayMessage={mockDisplayMessage}
+        />
+      )
+      getByTestId = component.getByTestId
+      getByText = component.getByText
+    })
+
+    it('should start game by calling changePage("intro")', () => {
+      setTimeout(async () => {
+        const startPage = getByTestId('start-page')
+        const startButton = await waitForElement(() =>
+          getByText(startPage, 'Start')
+        )
+
+        fireEvent.click(startButton)
+        expect(mockChangePage).toHaveBeenCalledWith('intro')
+      }, 5000)
+    })
+
+    it('should show credits for game by calling changePage("credits")', () => {
+      setTimeout(async () => {
+        const startPage = getByTestId('start-page')
+        const creditsButton = await waitForElement(() =>
+          getByText(startPage, 'Start')
+        )
+
+        fireEvent.click(creditsButton)
+        expect(mockChangePage).toHaveBeenCalledWith('credits')
+      }, 5000)
+    })
+  })
+})
