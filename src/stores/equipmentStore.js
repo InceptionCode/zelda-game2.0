e60b516111934e81e05eb915bf1898da84c530a2
Darrell Washington <darrellwjr.com@gmail.com>
2019-02-21 15:09:38 -0500

    Added reset case to equipmentReducer

--- src/stores/equipmentStore.js
+++ src/stores/equipmentStore.js
@@ -6,6 +6,8 @@ export default function EquipmentReducer(state, action) {
       return [...state].filter(item => item !== action.payload)
     case 'addEquipment':
       return state
+    case 'reset':
+      return ['sword', 'rope', 'flashlight', 'pen', 'hook']
     default:
       return state
   }
